da9afa2aaae6e5d6d2c6c5d6484e064f2fe25e21
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2022-12-01 13:06:59 +0000

    vars and styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Merriweather } from "@next/font/google";
+import { Space_Mono } from "@next/font/google";
+
+const merriweather = Merriweather({
+  weight: "400",
+  subsets: ["latin"],
+});
+const spaceMono = Space_Mono({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --type-primary: ${merriweather.style.fontFamily};
+          --type-secondary: ${spaceMono.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,32 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.scss";
 import Button from "../components/Button/Button";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Patient Derived Cancer Models</title>
         <meta name="description" content="Patient Derived Cancer Models" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={`${styles.title} h1Cypress`}>
-          Patient Derived Cancer Models
-        </h1>
+      <main>
+        <h1>Patient Derived Cancer Models</h1>
+        <h2>Patient Derived Cancer Models</h2>
+        <h3>Patient Derived Cancer Models</h3>
+        <h4>Patient Derived Cancer Models</h4>
+        <p>
+          Lorem ipsum dolor <a href="https://google.com">asdf sf asdf as</a> sit
+          amet
+        </p>
+        <a href="" className="link--text--light">
+          asdf sf asdf as
+        </a>
         <Button text="Don't click me" />
       </main>
 
-      <footer className={styles.footer}>
+      <footer>
         <p>Patient Derived Cancer Models</p>
       </footer>
     </div>
