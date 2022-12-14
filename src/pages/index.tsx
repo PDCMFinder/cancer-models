b8b5fa92d458ca39ce349360acc039622d434a25
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2022-12-14 15:17:56 +0000

    remove extra elements from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<div className="container">
-				<div className="row">
-					<div className="col-12">
-						<h1>asdf</h1>
-						<h2>asdf</h2>
-						<h3>asdf</h3>
-						<p>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
-							itaque ex dolore nisi nostrum dolorum, dolorem, cum, sapiente eum
-							officia provident error adipisci veniam! Reiciendis quae ipsam
-							libero quos quam?
-						</p>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+	return <></>;
 };
 
 export default Home;
