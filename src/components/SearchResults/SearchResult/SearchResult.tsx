c5caa4a1a83e312129c8ce04ca066c602d72ea76
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-01-17 17:01:11 +0000

    link each result card

--- src/components/SearchResults/SearchResult/SearchResult.tsx
+++ src/components/SearchResults/SearchResult/SearchResult.tsx
@@ -1,6 +1,7 @@
 import Card from "../../Card/Card";
 import styles from "./SearchResult.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 
 interface ISearchResultProps {
 	className?: string;
@@ -19,85 +20,100 @@ interface ISearchResultProps {
 }
 
 const SearchResult = (props: ISearchResultProps) => {
-	let data = props.data;
-	const organoid = "organoid";
+	let {
+		patient_age,
+		patient_sex,
+		external_model_id,
+		model_type,
+		data_source,
+		histology,
+		primary_site,
+		collection_site,
+		tumour_type,
+		dataset_available,
+	} = props.data;
 
 	return (
-		<Card
-			className={`bg-primary-tertiary ${props.className}`.trim()}
-			contentClassName="masonry_content"
+		<Link
+			href={`${data_source}/${external_model_id}`}
+			className={`${props.className}`.trim()}
 		>
 			<Card
-				className={`bg-white bc-primary-quaternary ${styles.SearchResult_titleCard}`}
-				contentClassName={`d-flex align-center justify-content-between ${styles.SearchResult_titleCard_content}`}
+				className={`bg-primary-tertiary`.trim()}
+				contentClassName="masonry_content"
 			>
-				<>
-					<h2 className="m-0">{data.histology}</h2>
-					<Image
-						src={`/${data.model_type}_icon-small.svg`}
-						alt={`Icon for ${data.model_type} model type`}
-						width={28}
-						height={28}
-					/>
-				</>
+				<Card
+					className={`bg-white bc-primary-quaternary ${styles.SearchResult_titleCard}`}
+					contentClassName={`d-flex align-center justify-content-between ${styles.SearchResult_titleCard_content}`}
+				>
+					<>
+						<h2 className="m-0">{histology}</h2>
+						<Image
+							src={`/${model_type}_icon-small.svg`}
+							alt={`Icon for ${model_type} model type`}
+							width={28}
+							height={28}
+						/>
+					</>
+				</Card>
+				<div
+					className={`text-white d-flex flex-column ${styles.SearchResult_dataContent}`}
+				>
+					<div className="row">
+						<div className="col-12">
+							<p className={`${styles.SearchResult_modelID}`}>
+								{data_source} / {external_model_id}
+							</p>
+							<p className="mb-0">{tumour_type}</p>
+						</div>
+					</div>
+					<div className={`row ${styles.SearchResult_metadata}`}>
+						<div className="col-6 col-lg-3">
+							<p className="text-capitalize">
+								<span>Primary site</span>
+								<br />
+								{primary_site}
+							</p>
+						</div>
+						<div className="col-6 col-lg-3">
+							<p className="text-capitalize">
+								<span>Patient age</span>
+								<br />
+								{patient_age}
+							</p>
+						</div>
+						<div className="col-6 col-lg-3">
+							<p className="text-capitalize">
+								<span>Patient sex</span>
+								<br />
+								{patient_sex}
+							</p>
+						</div>
+						<div className="col-6 col-lg-3">
+							<p className="text-capitalize">
+								<span>Collection site</span>
+								<br />
+								{collection_site.replaceAll("/", " / ")}
+							</p>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-12">
+							<p
+								className={`text-center ${styles.SearchResult_availableData_title}`}
+							>
+								Available data
+							</p>
+							<ul className="ul-noStyle ul-twoCols m-0 text-capitalize">
+								{dataset_available.map((dataset) => (
+									<li key={dataset}>{dataset}</li>
+								))}
+							</ul>
+						</div>
+					</div>
+				</div>
 			</Card>
-			<div
-				className={`text-white d-flex flex-column ${styles.SearchResult_dataContent}`}
-			>
-				<div className="row">
-					<div className="col-12">
-						<p className={`${styles.SearchResult_modelID}`}>
-							{data.data_source} / {data.external_model_id}
-						</p>
-						<p className="mb-0">{data.tumour_type}</p>
-					</div>
-				</div>
-				<div className={`row ${styles.SearchResult_metadata}`}>
-					<div className="col-6 col-lg-3">
-						<p className="text-capitalize">
-							<span>Primary site</span>
-							<br />
-							{data.primary_site}
-						</p>
-					</div>
-					<div className="col-6 col-lg-3">
-						<p className="text-capitalize">
-							<span>Patient age</span>
-							<br />
-							{data.patient_age}
-						</p>
-					</div>
-					<div className="col-6 col-lg-3">
-						<p className="text-capitalize">
-							<span>Patient sex</span>
-							<br />
-							{data.patient_sex}
-						</p>
-					</div>
-					<div className="col-6 col-lg-3">
-						<p className="text-capitalize">
-							<span>Collection site</span>
-							<br />
-							{data.collection_site.replaceAll("/", " / ")}
-						</p>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-12">
-						<p
-							className={`text-center ${styles.SearchResult_availableData_title}`}
-						>
-							Available data
-						</p>
-						<ul className="ul-noStyle ul-twoCols m-0 text-capitalize">
-							{data.dataset_available.map((dataset) => (
-								<li key={dataset}>{dataset}</li>
-							))}
-						</ul>
-					</div>
-				</div>
-			</div>
-		</Card>
+		</Link>
 	);
 };
 
