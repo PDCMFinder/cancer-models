2a1ca539e1edd008146559ddc8e3ec290c3f9acb
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-03-03 14:32:29 +0000

    getprocessedcontent adaptation for new type of content, include files

--- src/utils/getProcessedContent.ts
+++ src/utils/getProcessedContent.ts
@@ -1,8 +1,10 @@
 import { remark } from "remark";
-import html from "remark-html";
+import remarkHtml from "remark-html";
 import { promises as fs } from "fs";
 import path from "path";
 import matter from "gray-matter";
+import remarkGfm from "remark-gfm";
+import remarkUnwrapImages from "remark-unwrap-images";
 
 export const getProcessedContent = async (fileName: string) => {
 	const filePath = path.join(
@@ -12,8 +14,10 @@ export const getProcessedContent = async (fileName: string) => {
 	const fileContents = await fs.readFile(filePath, "utf8");
 	const matterResult = await matter(fileContents);
 	const processedContent = await remark()
-		.use(html)
-		.process(matterResult.content);
+		.use(remarkHtml, { sanitize: true })
+		.use(remarkGfm)
+		.use(remarkUnwrapImages)
+		.process(await matterResult.content);
 
 	return JSON.parse(JSON.stringify(processedContent.value)) as string;
 };
