29e03121628d64f7d7c8214ab45fc744d38d853d
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-03-08 10:25:09 +0000

    consent layout

--- src/components/CookieConsent/CookieConsent.tsx
+++ src/components/CookieConsent/CookieConsent.tsx
@@ -1,34 +1,53 @@
 import styles from "./CookieConsent.module.scss";
 import { useCookies } from "react-cookie";
+import Link from "next/link";
+import Button from "../Button/Button";
 
 interface ICookieConsentProps {}
 
 const CookieConsent = (props: ICookieConsentProps) => {
-	const [cookies, setCookie, removeCookie] = useCookies(["cookieConsent"]);
+	const [cookie, setCookie] = useCookies(["CookieConsent"]);
 
 	return (
-		<>
-			<div
-				onClick={() =>
-					setCookie("cookieConsent", "true", {
-						sameSite: "lax",
-						maxAge: 900000,
-					})
-				}
-			>
-				cookie?
+		<div
+			className={`${styles.CookieConsent} position-fixed bottom-0 w-100 d-md-flex align-center bg-white p-2`}
+		>
+			<p className="mb-0">
+				This website requires cookies, and the limited processing of your
+				personal data in order to function. By using the site you are agreeing
+				to this as outlined in our{" "}
+				<Link href="/privacy-policy">Privacy Notice</Link> and{" "}
+				<Link href="/terms-of-use">Terms of Use</Link>.
+			</p>
+			<div className="ml-md-2 d-lg-flex">
+				<Button
+					className="mb-0 mt-md-0"
+					priority="secondary"
+					color="dark"
+					onClick={() =>
+						setCookie("CookieConsent", "false", {
+							sameSite: "lax",
+							maxAge: 900000,
+						})
+					}
+				>
+					Reject
+				</Button>
+				<Button
+					className="ml-3 ml-md-0 ml-lg-2 mb-0 mt-lg-0"
+					priority="primary"
+					color="dark"
+					onClick={() =>
+						setCookie("CookieConsent", "true", {
+							sameSite: "lax",
+							maxAge: 900000,
+						})
+					}
+				>
+					Accept
+				</Button>
 			</div>
-			<div
-				onClick={() =>
-					setCookie("cookieConsent", "false", {
-						sameSite: "lax",
-						maxAge: 900000,
-					})
-				}
-			>
-				no cookie.
-			</div>
-		</>
+		</div>
 	);
 };
 
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,20 +2,19 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import CookieConsent from "../CookieConsent/CookieConsent";
 import { useCookies } from "react-cookie";
-import { useEffect } from "react";
 
 interface ILayoutProps {
 	children: JSX.Element;
 }
 
 const Layout = (props: ILayoutProps) => {
-	const [cookies] = useCookies(["cookieConsent"]);
+	const [cookies] = useCookies(["CookieConsent"]);
 
 	return (
 		<>
 			<Navbar />
 			<main>{props.children}</main>
-			{!cookies["cookieConsent"] && <CookieConsent />}
+			{!cookies["CookieConsent"] && <CookieConsent />}
 			<Footer />
 		</>
 	);
