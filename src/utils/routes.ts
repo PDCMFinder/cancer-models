6c846fd3efd893edc2a5d9067da1d4ece092aecf
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-03-17 14:09:05 +0000

    change PDCM Finder menu item, hide metadata dict, hide faq

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -10,16 +10,16 @@ export const routes = [
 		children: [
 			{
 				path: "/about",
-				name: "About PDCM Finder",
+				name: "PDCM Finder",
 			},
-			{
-				path: "/about/metadata-dictionary",
-				name: "Metadata Dictionary",
-			},
-			{
-				path: "/about/faq",
-				name: "FAQ",
-			},
+			// {
+			// 	path: "/about/metadata-dictionary",
+			// 	name: "Metadata Dictionary",
+			// },
+			// {
+			// 	path: "/about/faq",
+			// 	name: "FAQ",
+			// },
 			{
 				path: "/contact",
 				name: "Contact",
