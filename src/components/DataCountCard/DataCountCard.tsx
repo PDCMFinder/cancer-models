83d42dc4bf6988a46cce6b6f1fbcd9614f49708d
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-03-22 15:39:54 +0000

    countcard fix

--- src/components/DataCountCard/DataCountCard.tsx
+++ src/components/DataCountCard/DataCountCard.tsx
@@ -11,37 +11,33 @@ const DataCountCard = () => {
 		return getModelsByType();
 	});
 
-	return (
-		<div className="col-12 col-lg-3 col-xl-2 offset-lg-1 offset-xl-2">
-			{modelsByTypeCountsQuery.data ? (
-				<Card className="bg-primary-quaternary">
-					<div className="row text-center justify-content-center">
-						{modelsByTypeCountsQuery.data
-							?.filter((d) => d.modelType !== "other")
-							.map((d) => (
-								<div
-									className={`${styles.DataCountCard_item} col-6 col-lg-12`}
-									key={d.modelType}
-								>
-									<Link
-										href={`/search?filters=model_type:${d.modelType}`}
-										className="p text-noDecoration"
-									>
-										<p className="h2 mb-0">
-											{parseFloat(d.count).toLocaleString()}
-										</p>
-										<p className="text-underline">
-											{capitalizeFirstLetter(d.modelType)} models
-										</p>
-									</Link>
-								</div>
-							))}
-					</div>
-				</Card>
-			) : (
-				<Loader />
-			)}
-		</div>
+	return modelsByTypeCountsQuery.data ? (
+		<Card className="bg-primary-quaternary">
+			<div className="row text-center justify-content-center">
+				{modelsByTypeCountsQuery.data
+					?.filter((d) => d.modelType !== "other")
+					.map((d) => (
+						<div
+							className={`${styles.DataCountCard_item} col-6 col-lg-12`}
+							key={d.modelType}
+						>
+							<Link
+								href={`/search?filters=model_type:${d.modelType}`}
+								className="p text-noDecoration"
+							>
+								<p className="h2 mb-0">
+									{parseFloat(d.count).toLocaleString()}
+								</p>
+								<p className="text-underline">
+									{capitalizeFirstLetter(d.modelType)} models
+								</p>
+							</Link>
+						</div>
+					))}
+			</div>
+		</Card>
+	) : (
+		<Loader />
 	);
 };
 
