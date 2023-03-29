8f0b145abf09694527ffd0d8d6b18dcdef457e0e
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-03-29 12:36:43 +0100

    add hotjar script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Cookies, CookiesProvider } from "react-cookie";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import Script from "next/script";
 
 const USERNAVIGATION_MOUSE = "userNavigation-mouse",
 	USERNAVIGATION_KEYBOARD = "userNavigation-keyboard",
@@ -103,6 +104,18 @@ function CancerModels({ Component, pageProps, cookies }: AppProps) {
 						href="/favicon-180.png"
 						sizes="180x180"
 					/>
+
+					{/* Hotjar Tracking Code for Cancer Models Org */}
+					<Script>
+						{`(function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3209855,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+					</Script>
 				</Head>
 				<style jsx global>{`
 					:root {
