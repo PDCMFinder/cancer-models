486ddbba69cbaef08d869811c6d1bdb22521c944
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-04-03 08:30:51 +0100

    remove hotjar script from dev subdomain

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,6 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Cookies, CookiesProvider } from "react-cookie";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
-import Script from "next/script";
 
 const USERNAVIGATION_MOUSE = "userNavigation-mouse",
 	USERNAVIGATION_KEYBOARD = "userNavigation-keyboard",
@@ -105,17 +104,6 @@ function CancerModels({ Component, pageProps, cookies }: AppProps) {
 						sizes="180x180"
 					/>
 				</Head>
-				{/* Hotjar Tracking Code for Cancer Models Org */}
-				<Script id="hotjar">
-					{`(function(h,o,t,j,a,r){
-                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:3209855,hjsv:6};
-                  a=o.getElementsByTagName('head')[0];
-                  r=o.createElement('script');r.async=1;
-                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                  a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-				</Script>
 				<style jsx global>{`
 					:root {
 						--type-primary: ${merriweather.style.fontFamily}, serif;
