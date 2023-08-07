6a535d08ec915dbbdfc059f64da3c0a5b296d908
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-08-07 11:02:26 +0100

    add tooltip

--- src/components/QualityBadge/QualityBadge.tsx
+++ src/components/QualityBadge/QualityBadge.tsx
@@ -1,4 +1,5 @@
 import { CSSProperties } from "react";
+import Tooltip from "../Tooltip/Tooltip";
 
 interface IQualityBadgeProps {
 	className?: string;
@@ -10,16 +11,18 @@ interface IQualityBadgeProps {
 const QualityBadge = (props: IQualityBadgeProps) => {
 	return (
 		<div className={props.containerClassName}>
-			<meter
-				className={props.className}
-				min={0}
-				max={100}
-				value={props.score}
-				high={70}
-				low={30}
-				optimum={90}
-				style={props.style}
-			></meter>
+			<Tooltip content={<span className="text-small">{props.score}</span>}>
+				<meter
+					className={props.className}
+					min={0}
+					max={100}
+					value={props.score}
+					high={70}
+					low={30}
+					optimum={90}
+					style={props.style}
+				></meter>
+			</Tooltip>
 			<p className="text-small m-0">Model characterisation</p>
 		</div>
 	);
