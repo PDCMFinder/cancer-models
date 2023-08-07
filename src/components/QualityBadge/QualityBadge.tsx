8e960ed0e36286f3e00d281b98b769097c324237
mriios <marcelo10rios@hotmail.com>
2023-08-07 11:31:14 +0100

    Revert "Show score on quality badge hover"

--- src/components/QualityBadge/QualityBadge.tsx
+++ src/components/QualityBadge/QualityBadge.tsx
@@ -1,5 +1,4 @@
 import { CSSProperties } from "react";
-import Tooltip from "../Tooltip/Tooltip";
 
 interface IQualityBadgeProps {
 	className?: string;
@@ -11,18 +10,16 @@ interface IQualityBadgeProps {
 const QualityBadge = (props: IQualityBadgeProps) => {
 	return (
 		<div className={props.containerClassName}>
-			<Tooltip content={<span className="text-small">{props.score}</span>}>
-				<meter
-					className={props.className}
-					min={0}
-					max={100}
-					value={props.score}
-					high={70}
-					low={30}
-					optimum={90}
-					style={props.style}
-				></meter>
-			</Tooltip>
+			<meter
+				className={props.className}
+				min={0}
+				max={100}
+				value={props.score}
+				high={70}
+				low={30}
+				optimum={90}
+				style={props.style}
+			></meter>
 			<p className="text-small m-0">Model characterisation</p>
 		</div>
 	);
