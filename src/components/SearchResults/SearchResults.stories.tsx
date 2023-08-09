a22bb245f40941fa68ef949dd1a702daa9582e13
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-08-09 11:32:09 +0100

    add compare check to searchresults

--- src/components/SearchResults/SearchResults.stories.tsx
+++ src/components/SearchResults/SearchResults.stories.tsx
@@ -1,19 +1,40 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import SearchResults from "./SearchResults";
 import mockData from "./StoryMockData";
+import { useState } from "react";
 
 export default {
 	title: "UI/SearchResults",
 	component: SearchResults,
 } as ComponentMeta<typeof SearchResults>;
 
-const Template: ComponentStory<typeof SearchResults> = (args) => (
-	<SearchResults {...args} />
-);
+const Template: ComponentStory<typeof SearchResults> = (args) => {
+	const [modelsToCompare, setModelsToCompare] = useState<string[]>([]);
+
+	const compareModel = (id: string): void => {
+		if (modelsToCompare.includes(id)) {
+			setModelsToCompare((prev) => prev.filter((model) => model !== id));
+		} else {
+			if (modelsToCompare.length === 4) {
+				alert(
+					"You've reached the maximum amount of models to compare. Remove a model to add another."
+				);
+			} else {
+				setModelsToCompare((prev) => [...prev, id]);
+			}
+		}
+	};
+
+	return (
+		<SearchResults
+			{...args}
+			compareModel={compareModel}
+			modelsToCompare={modelsToCompare}
+		/>
+	);
+};
 
 export const Component = Template.bind({});
 Component.args = {
-	modelsToCompare: [],
-	compareModel: () => {},
 	data: mockData,
 };
