088259d47e49ad7cab91321ef18bfc86d31c43d7
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-09-01 17:16:24 +0100

    z-index, copyable

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -15,7 +15,7 @@ const Tooltip = (props: ITooltipProps) => {
 			<div
 				onMouseOver={() => setIsHovering(true)}
 				onMouseLeave={() => setIsHovering(false)}
-				className="d-inline position-relative"
+				className="d-inline position-relative pr-2"
 			>
 				{isHovering && (
 					<>
