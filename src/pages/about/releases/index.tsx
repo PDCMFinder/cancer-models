2c30177c51463c0d0ffc43bc00d3077e85fe6570
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-10-31 10:30:57 +0000

    release filter

--- src/pages/about/releases/index.tsx
+++ src/pages/about/releases/index.tsx
@@ -11,6 +11,7 @@ import { useEffect, useState } from "react";
 import Card from "../../../components/Card/Card";
 import styles from "./index.module.scss";
 import { IGitlabRelease } from "../../../../types/releaseTypes";
+import InputAndLabel from "../../../components/Input/InputAndLabel";
 
 interface IReleasesProps {}
 
@@ -18,9 +19,12 @@ const Releases: NextPage<IReleasesProps> = () => {
 	const [parsedDataReleases, setParsedDataReleases] = useState<
 		IGitlabRelease[]
 	>([]);
+	const [showDataReleases, setShowDataReleases] = useState<boolean>(true);
 	const [parsedUIReleases, setParsedUIReleases] = useState<IGitlabRelease[]>(
 		[]
 	);
+	const [showUIReleases, setShowUIReleases] = useState<boolean>(true);
+	const [parsedReleases, setParsedReleases] = useState<IGitlabRelease[]>([]);
 
 	const parseReleaseContent = async (
 		release: IGitlabRelease,
@@ -75,6 +79,37 @@ const Releases: NextPage<IReleasesProps> = () => {
 		}
 	);
 
+	const sortReleases = (x: IGitlabRelease[], y: IGitlabRelease[]) => {
+		return [...x, ...y].sort(
+			(a, b) =>
+				new Date(b.released_at).getTime() - new Date(a.released_at).getTime()
+		);
+	};
+
+	useEffect(() => {
+		setParsedReleases(sortReleases(parsedDataReleases, parsedUIReleases));
+	}, [parsedDataReleases, parsedUIReleases]);
+
+	const handleFilter = (filter?: string) => {
+		let data: [] | IGitlabRelease[] = showDataReleases
+			? parsedDataReleases
+			: [];
+		let ui: [] | IGitlabRelease[] = showUIReleases ? parsedUIReleases : [];
+
+		if (filter === "data") {
+			let showDataStatus = !showDataReleases;
+			data = showDataStatus ? parsedDataReleases : [];
+			setShowDataReleases(showDataStatus);
+		}
+		if (filter === "ui") {
+			let showUIStatus = !showUIReleases;
+			ui = showUIStatus ? parsedUIReleases : [];
+			setShowUIReleases(showUIStatus);
+		}
+
+		setParsedReleases(sortReleases(data, ui));
+	};
+
 	return (
 		<>
 			<header className="bg-primary-primary text-white mb-5 py-5">
@@ -88,38 +123,57 @@ const Releases: NextPage<IReleasesProps> = () => {
 			</header>
 			<section>
 				<div className="container">
-					{parsedUIReleases.length > 0 || parsedDataReleases.length > 0 ? (
-						[...parsedDataReleases, ...parsedUIReleases]
-							.sort(
-								(a, b) =>
-									new Date(b.released_at).getTime() -
-									new Date(a.released_at).getTime()
-							)
-							.map((data: IGitlabRelease) => (
-								<div className="row mb-5" key={data.released_at}>
-									<div className="col-12 col-lg-8 offset-lg-2">
-										<Card
-											headerClassName="py-1"
-											header={
-												<div className="d-flex align-center justify-content-between">
-													<h2 className="p-0 m-0 h3">
-														{data.repository}:{" "}
-														<span className="text-bold">{data.tag_name}</span>
-													</h2>
-													<p className="h4 m-0">{data.released_at}</p>
-												</div>
-											}
-											contentClassName="py-2"
-										>
-											<div
-												className={styles.Releases_release_content}
-												// we trust gitlab and github, dont we? Nothing dangerous here
-												dangerouslySetInnerHTML={{ __html: data.description }}
-											/>
-										</Card>
-									</div>
+					<div className="row mb-5">
+						<div className="col-12 col-lg-8 offset-lg-2">
+							<div className="bg-lightGray p-2 br-common">
+								<div className="d-flex align-center justify-content-around">
+									<h2 className="m-0">Show:</h2>
+									<InputAndLabel
+										forId="filter-dataReleases"
+										label="Data releases"
+										name="filter-data"
+										type="checkbox"
+										checked={showDataReleases}
+										onChange={() => handleFilter("data")}
+									/>
+									<InputAndLabel
+										forId="filter-uiReleases"
+										label="UI releases"
+										name="filter-ui"
+										type="checkbox"
+										checked={showUIReleases}
+										onChange={() => handleFilter("ui")}
+									/>
 								</div>
-							))
+							</div>
+						</div>
+					</div>
+					{parsedUIReleases.length > 0 || parsedDataReleases.length > 0 ? (
+						parsedReleases.map((data: IGitlabRelease) => (
+							<div className="row mb-5" key={data.released_at + data.tag_name}>
+								<div className="col-12 col-lg-8 offset-lg-2">
+									<Card
+										headerClassName="py-1"
+										header={
+											<div className="d-flex align-center justify-content-between">
+												<h2 className="p-0 m-0 h3">
+													{data.repository}:{" "}
+													<span className="text-bold">{data.tag_name}</span>
+												</h2>
+												<p className="h4 m-0">{data.released_at}</p>
+											</div>
+										}
+										contentClassName="py-2"
+									>
+										<div
+											className={styles.Releases_release_content}
+											// we trust gitlab and github, dont we? Nothing dangerous here
+											dangerouslySetInnerHTML={{ __html: data.description }}
+										/>
+									</Card>
+								</div>
+							</div>
+						))
 					) : (
 						<div style={{ height: "300px" }}>
 							<Loader />
