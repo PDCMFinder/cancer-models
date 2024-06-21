0abc21490a2941c251bfcae79360bede3b2eade3
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2024-06-21 16:37:44 +0100

    add facet operators to search result deps

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -255,7 +255,8 @@ const Search: NextPage = () => {
 				searchValues: [],
 				searchFilterState,
 				resultsPerPage,
-				sortBy
+				sortBy,
+				searchFacetOperators
 			}
 		],
 		queryFn: async () =>
@@ -266,7 +267,8 @@ const Search: NextPage = () => {
 				sortBy,
 				searchFacetOperators
 			),
-		enabled: searchFilterState !== null || searchFacetOperators.length > 0 // Only enable when `searchFilterState` and facet operators are ready
+		enabled:
+			searchFilterState !== null || Boolean(searchFacetOperatorsQuery.data) // Only enable when `searchFilterState` and facet operators are ready
 	});
 
 	useEffect(() => {
