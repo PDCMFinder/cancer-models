1b4eba2f762dc73ac81be4e4e9e89355e67677ab
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2024-09-12 15:13:52 -0600

    node backgrounds by type

--- src/utils/parseKnowledgeGraph.ts
+++ src/utils/parseKnowledgeGraph.ts
@@ -36,7 +36,7 @@ const parseKnowledgeGraph = (
 			},
 			className: `${
 				node.nodeLabel === currentModelId && node.data?.type ? "current" : ""
-			} ${node.nodeType ? `type-${node.nodeType}` : ""}`,
+			} ${node.nodeType ? `nodeType-${node.nodeType}` : ""}`,
 			...commonNodeProperties
 		})),
 		edges: data.edges.map((edge) => ({
