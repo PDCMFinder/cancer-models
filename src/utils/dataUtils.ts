57910fa11d3062e42849e1f845fd413c482124f8
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2024-09-18 11:53:16 -0600

    cleanup

--- src/utils/dataUtils.ts
+++ src/utils/dataUtils.ts
@@ -1,5 +1,3 @@
-// import { CamelCaseKeys } from "../../types/globalTypes";
-
 type CamelCase<S extends string> =
 	S extends `${infer P1}_${infer P2}${infer P3}`
 		? `${Lowercase<P1>}${Uppercase<P2>}${CamelCase<P3>}`
