f2bf7e773de7551166365403b34966452cc0805c
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2025-06-25 09:44:26 -0600

    add banner to layout

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -5,22 +5,21 @@ const Banner = () => {
 				<div className="row">
 					<div className="col-12 text-center">
 						<p className="text-family-primary">
-							Do data resources managed by EMBL-EBI and our collaborators make a
-							difference to your work?
+							We need your help! Has CancerModels.Org saved you time or effort?
 						</p>
 						<p className="text-small">
-							Please take 10 minutes to fill in our annual user survey, and help
-							us make the case for why sustaining open data resources is
-							critical for life sciences research.
+							Please take 15 minutes to fill in a survey and help EMBL-EBI make
+							the case for why open data resources are critical to life science
+							research.
 						</p>
 						<p className="mb-0 text-small">
 							Survey link:{" "}
 							<a
-								href="https://www.surveymonkey.com/r/HJKYKTT?channel=[website]"
+								href="https://www.surveymonkey.com/r/QGFMBH8?channel=[webpage] "
 								target="_blank"
 								rel="noopener noreferrer"
 							>
-								https://www.surveymonkey.com/r/HJKYKTT
+								https://www.surveymonkey.com/r/QGFMBH8
 							</a>
 						</p>
 					</div>
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import ReactGA from "react-ga4";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import Banner from "../Banner/Banner";
 import Card from "../Card/Card";
 import CookieConsent from "../CookieConsent/CookieConsent";
 import FloatingButton from "../FloatingWidget/FloatingButton";
@@ -41,6 +42,7 @@ const Layout = (props: LayoutProps) => {
 	return (
 		<>
 			<QueryClientProvider client={queryClient}>
+				<Banner />
 				<Navbar />
 				<main>{props.children}</main>
 				<CookieConsent />
