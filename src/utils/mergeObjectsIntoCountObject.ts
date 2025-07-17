af909938f2dbf3d9bfedb39d112c3e3313f68235
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2025-07-17 17:42:08 +0100

    add count instead of replace (#629)

--- src/utils/mergeObjectsIntoCountObject.ts
+++ src/utils/mergeObjectsIntoCountObject.ts
@@ -4,11 +4,11 @@ const mergeObjectsIntoCountObject = <T extends string>(
 	return obj.reduce(
 		(acc: Record<string, number>, curr: Record<string, number>) => {
 			const key = Object.keys(curr)[0] as T; // Extract the first key so we can use it as the key in the result object
-			acc[curr[key]] = curr.count;
+			acc[curr[key]] = (acc[curr[key]] || 0) + curr.count;
 
 			return acc;
 		},
-		{}
+		{} as Record<T, number>
 	);
 };
 
